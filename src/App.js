10b15ada1fd4a44e9cd382d6543ccb56066eeeb8
Nick Boober <nboober@terpmail.umd.edu>
2020-02-12 12:28:51 -0500

    started adding function calls on click of the buttons

--- src/App.js
+++ src/App.js
@@ -10,30 +10,39 @@ class App extends React.Component{
       valueTwo: 0
     }
   }
+
+  backSpace = () => {
+    console.log("backspace")
+  }
+
+  number = (event) => {
+    console.log(event.target.innerText)
+  }
+
   render(){
     return(
       <div className="parent">
-        <div className="div1">0</div>
+        <div className="div1">{this.state.total}</div>
         <div className="div2">%</div>
         <div className="div3">CE</div>
         <div className="div4">C</div>
-        <div className="div5">BackSpace</div>
-        <div className="div6">7</div>
-        <div className="div7">8</div>
-        <div className="div8">9</div>
-        <div className="div9">X</div>
-        <div className="div10">4</div>
-        <div className="div11">5</div>
-        <div className="div12">6</div>
-        <div className="div13">-</div>
-        <div className="div14">1</div>
-        <div className="div15">2</div>
-        <div className="div16">3</div>
-        <div className="div17">+</div>
-        <div className="div18">+/-</div>
-        <div className="div19">0</div>
-        <div className="div20">.</div>
-        <div className="div21">=</div>
+        <div className="div5" onClick={this.backSpace}>BackSpace</div>
+        <div className="div6" onClick={this.number}>7</div>
+        <div className="div7" onClick={this.number}>8</div>
+        <div className="div8" onClick={this.Number}>9</div>
+        <div className="div9" onClick={this.calculate}>X</div>
+        <div className="div10" onClick={this.number}>4</div>
+        <div className="div11" onClick={this.number}>5</div>
+        <div className="div12" onClick={this.number}>6</div>
+        <div className="div13" onClick={this.calculate}>-</div>
+        <div className="div14" onClick={this.number}>1</div>
+        <div className="div15" onClick={this.number}>2</div>
+        <div className="div16" onClick={this.number}>3</div>
+        <div className="div17" onClick={this.calculate}>+</div>
+        <div className="div18" onClick={this.calculate}>/</div>
+        <div className="div19" onClick={this.number}>0</div>
+        <div className="div20" >.</div>
+        <div className="div21" >=</div>
       </div>
     )
   }
