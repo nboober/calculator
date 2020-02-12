5af90ec71ed163b52f5562bd2ec00e923449bf1d
Nick Boober <nboober@terpmail.umd.edu>
2020-02-12 12:04:33 -0500

    added calculator layout in html and css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return(
+      <div className="parent">
+        <div className="div1">0</div>
+        <div className="div2">%</div>
+        <div className="div3">CE</div>
+        <div className="div4">C</div>
+        <div className="div5">BackSpace</div>
+        <div className="div6">7</div>
+        <div className="div7">8</div>
+        <div className="div8">9</div>
+        <div className="div9">X</div>
+        <div className="div10">4</div>
+        <div className="div11">5</div>
+        <div className="div12">6</div>
+        <div className="div13">-</div>
+        <div className="div14">1</div>
+        <div className="div15">2</div>
+        <div className="div16">3</div>
+        <div className="div17">+</div>
+        <div className="div18">+/-</div>
+        <div className="div19">0</div>
+        <div className="div20">.</div>
+        <div className="div21">=</div>
+      </div>
+    )
+  }
 }
 
 export default App;
